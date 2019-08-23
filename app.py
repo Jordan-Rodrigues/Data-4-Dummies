5cc7e82009c9e393dff15b362fb2c7789acced72
Jordan Rodrigues <jrodrigues0801@gmail.com>
2019-08-23 09:18:54 -0400

    updating changes

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
+import pandas as pd
+import matplotlib.pyplot as plt 
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,6 +12,8 @@ def homepage():
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+      csvFile = request.files['file']
+      df = pd.read_csv(csvFile)
+      dfhead = df.head(7)
+      dfhtml = dfhead.to_html()
+      return render_template("mainData.html", dfhtml=dfhtml, fileName = csvFile.filename)
--- static/scripts/home.js
+++ static/scripts/home.js
@@ -1,15 +1,19 @@
+//global variables for page fading
 var globalPos = 0
 var pagePlace = 1
 
+//custom sleep function (asynchronous)
 const sleep = (milliseconds) => {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 
+//change color of items
 $(document).click(function () {
     $(".featureItem").css("background-color", "#266DD3")
     $(".featureItem").css("color", "white")
 })
 
+//change color on off click
 $(".featureItem").click(async function () {
     await sleep(1)
     $(".featureItem").css("background-color", "#266DD3")
@@ -20,12 +24,15 @@ $(".featureItem").click(async function () {
     $(".featDesc").html($(this).attr("id"))
 })
 
+//fade in submit button after select
 $("input").click(async function () {
     await sleep(1000)
     $(".submit").css("visibility", "visible")
     $(".submitImage").css("visibility", "visible")
 })
 
+
+//determines direction of scroll
 function scrollDirection() {
     var st = $(window).scrollTop();
     if (st > window.globalPos) {
@@ -35,6 +42,7 @@ function scrollDirection() {
     }
 };
 
+//animation function for scroll
 const animate2 = async (pos) => {
     $('html, body').animate({
         scrollTop: pos
@@ -45,6 +53,7 @@ const animate2 = async (pos) => {
     return new Promise(resolve => setTimeout(resolve, 1))
 }
 
+//calculates position of various sections dynamically
 function sec2Pos() {
     var featPos = $(".featTitle").offset().top
     var headerHeight = $(".headerTitle").height()
@@ -62,14 +71,17 @@ function sec21Pos() {
 }
 
 function sec3Pos() {
-    var waitPos = $("#upTitle").offset().top
+    var waitPos = $("#upTitle").offset().top * .9
     var headerHeight = $(".headerBar").height()
     var sec3pos = waitPos - headerHeight
     return sec3pos
 }
 
+//throttling the scroll call
 var throttled = _.throttle(pageSwitch, 2100, {trailing: false})
+$(window).scroll(throttled)
 
+//function to switch page
 async function pageSwitch() {
     var direction = scrollDirection()
     var sec2pos = sec2Pos()
@@ -77,30 +89,34 @@ async function pageSwitch() {
     var sec3pos = sec3Pos()
     if ((window.pagePlace == 1) && (direction == "down")) {
         $("#arrow1").fadeOut(1000)
+        greetingFade2()
         featureFade()
         await animate2(sec2pos)
         window.pagePlace = 2
     } else if ((window.pagePlace == 2) && (direction == "up")) {
+        greetingFade()
         featureFade2()
         $("#arrow1").fadeIn(1000)
         await animate2(0)
         window.pagePlace = 1
     } else if ((window.pagePlace == 2) && (direction == "down")) {
         featureFade2()
+        uploadFade()
         $("#arrow2").fadeOut(1000)
         await animate2(sec3pos)
         window.pagePlace = 3
     } else if ((window.pagePlace == 3) && (direction == "up")) {
         featureFade()
+        uploadFade2()
         $("#arrow2").fadeIn(1000)
         await animate2(sec21pos)
         window.pagePlace = 2
     }
 }
 
-$(window).scroll(throttled)
-
+//arrow functionality
 $("#arrow1").click(async function() {
+    greetingFade2()
     featureFade()
     $("#arrow1").fadeOut(1000)
     await animate2(sec2Pos())
@@ -116,10 +132,12 @@ $("#arrow2").click(async function() {
 
 }
 )
+
+//fade functions
 function featureFade() {
     $(".features").animate({
         opacity: 1
-    }, 3500)
+    }, 3000)
 }
 
 function featureFade2() {
@@ -127,3 +145,33 @@ function featureFade2() {
         opacity: 0
     }, 1000)
 }
+
+function greetingFade() {
+    $(".greeting").animate({
+        opacity: 1
+    }, 3000)
+}
+
+function greetingFade2() {
+    $(".greeting").animate({
+        opacity: 0
+    }, 1000)
+}
+
+function uploadFade() {
+    $(".upload").animate({
+        opacity: 1
+    }, 3000)
+}
+
+function uploadFade2() {
+    $(".upload").animate({
+        opacity: 0
+    }, 1000)
+
+}
+
+$('.fileUpload').change(function() {
+    console.log("test")
+    $(".fileName").text("Selected file: " + $('.fileUpload')[0].files[0].name)
+  });
