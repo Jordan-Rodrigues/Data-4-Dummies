4b9a84f7bbcf8f90f0349d828a525fc9debcf35e
Jordan Rodrigues <jrodrigues0801@gmail.com>
2019-12-12 22:49:49 -0500

    updates

--- static/scripts/mainData.js
+++ static/scripts/mainData.js
@@ -1,12 +1,13 @@
 $(".pageOption").hover(function() {
     $(this).css("color", "#0A369D")
     $(this).css("background-color", "white")
-    console.log(this.id)
+    $(".pageDesc").css("visibility", "visible")
     $(".pageDesc").html(this.id)
 })
 
 $(".pageOption").mouseleave(function() {
     $(this).css("color", "white")
     $(this).css("background-color", "#0A369D")
-    $(".pageDesc").html("")
+    $(".pageDesc").html("A")
+    $(".pageDesc").css("visibility", "hidden")
 }) 
